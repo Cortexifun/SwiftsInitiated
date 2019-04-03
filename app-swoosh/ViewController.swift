67b19cd9895312e44f79576a422ccc154945ab34
Omid <omidtavanaei@gmail.com>
2019-04-03 20:21:02 +0430

    add constraints

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -16,10 +16,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 0, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame
-    }
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 0, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//
+//        bgImg.frame = view.frame
+   }
 
 
 }
