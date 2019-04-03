f6942e6620ded48c7522aff6008c8886e4798e06
Omid <omidtavanaei@gmail.com>
2019-04-03 23:18:39 +0430

    MVC completed

---
+++ app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeVC.swift
+//  app-swoosh
+//
+//  Created by Omid on 03.04.19.
+//  Copyright © 2019 Omid. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+        @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+            
+            
+   }
+
+}
+
